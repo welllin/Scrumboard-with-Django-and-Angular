272347fc57fb77b1775d28c642ef8b5cebfa22c6
Well <wellup639936@gmail.com>
2016-11-11 23:17:07 +1300

    Fixed code cause version

--- scrumboard/serializers.py
+++ scrumboard/serializers.py
@@ -6,8 +6,10 @@ class ListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = List
+        fields = '__all__'
 
 class CardSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Card
+        fields = '__all__'
