bc2b58156349aeff6a65e3b0e38856fa09b0a218
Well <wellup639936@gmail.com>
2016-11-11 22:57:22 +1300

    Added a serializers to convert data to JSON format so that Angular can use it

---
+++ scrumboard/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from .models import List, Card
+
+class ListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = List
+
+class CardSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Card
