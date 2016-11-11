d94b69890ae9d7462ea4b01a91911658870ec300
Well <wellup639936@gmail.com>
2016-11-12 12:12:23 +1300

    Added moving functionality

--- static/js/card.directive.js
+++ static/js/card.directive.js
@@ -10,21 +10,27 @@
         restrict: 'E',
         controller: ['$scope', '$http', function($scope, $http){
           var url = '/scrumboard/cards/' + $scope.card.id + '/';
+          $scope.destList = $scope.list;
+
           $scope.update = function() {
-            $http.put(
+            return $http.put(
               url,
               $scope.card
             );
           };
 
+          function removeCardFromList(card, list) {
+            var cards = $scope.list.cards;
+            cards.splice(
+              cards.indexOf($scope.card),
+              1
+            )
+          }
+
           $scope.delete = function() {
             $http.delete(url).then(
               function(){
-                var cards = $scope.list.cards;
-                cards.splice(
-                  cards.indexOf($scope.card),
-                  1
-                )
+                removeCardFromList($scope.card, $scope.list);
               }
             )
           }
@@ -32,6 +38,20 @@
           $scope.modelOptions = {
             debounce: 500
           };
+
+          $scope.move = function() {
+            if ($scope.destList === undefined) {
+              return;
+            }
+
+            $scope.card.list = $scope.destList.id;
+            $scope.update().then(function() {
+              {
+                removeCardFromList($scope.card, $scope.list);
+                $scope.destList.cards.push($scope.card);
+              }
+            })
+          }
 
         }]
       };
