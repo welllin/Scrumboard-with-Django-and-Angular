8c74855e2ff35457a135ed9ae16696345b277695
Well <wellup639936@gmail.com>
2016-11-12 09:29:05 +1300

    Added Card Directive

---
+++ scrumboard/static/scrumboard/card.directive.js
@@ -0,0 +1,14 @@
+(function(){
+  'use strict';
+
+  angular.module('scrumboard.demo')
+    .directive('scrumboardCard', CardDirective);
+
+    function CardDirective() {
+      return {
+        templateUrl: '/static/scrumboard/card.html',
+        restrict: 'E'
+      };
+    }
+
+})();
