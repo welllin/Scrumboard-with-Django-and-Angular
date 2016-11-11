177fbdb518a43e10765debdc7669b357b16d337d
Well <wellup639936@gmail.com>
2016-11-11 22:45:47 +1300

    Create database & tables

--- scrumboard/models.py
+++ scrumboard/models.py
@@ -9,6 +9,9 @@ class List(models.Model):
 class Card(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    list = models.ForeignKey(List, related_name="cards")
+    story_points = models.IntegerField(null=True, blank=True)
+    business_value = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return "Card: {}".format(self.title)
